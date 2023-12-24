29d15e35f35a0b2081742c55f0d5fbfbaaabd146
Ajit Satpute <ajit.git174@gmail.com>
2023-12-24 11:20:53 +0530

    LogAPI creation

---
+++ Main/BackEnd/SingUpAPI/src/test/java/com/TestProject/zomato/SingUpAPI/SingUpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestProject.zomato.SingUpAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingUpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
