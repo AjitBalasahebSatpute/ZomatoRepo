d398cac970d36e56a60fe153cd7c27fd6605b2ee
Ajit Satpute <ajit.git174@gmail.com>
2023-12-25 18:35:42 +0530

    Added details component and routing for the same when user clicks on city

---
+++ Main/FrontEnd/src/app/details/main-content/main-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.css'
+})
+export class MainContentComponent {
+
+}
